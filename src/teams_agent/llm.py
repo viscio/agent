8a97ca76352c124a76e7a2cf6eadef825acbd7fa
Michele Visciarelli <m.visciarelli@cineca.it>
2025-09-08 11:26:40 +0200

    removed local credentials

--- src/teams_agent/llm.py
+++ src/teams_agent/llm.py
@@ -2,13 +2,8 @@ import os
 from typing import Optional
 from openai import OpenAI
 
-#BASE_URL = os.getenv("VLLM_BASE_URL", "http://localhost:8000/v1")
-BASE_URL = "http://131.175.207.215:8000/v1"
-
-#API_KEY = os.getenv("VLLM_API_KEY", "local-key")
-API_KEY = "hXpaz5&knrmJLsC5><Jrbgn"
-
-#MODEL = os.getenv("VLLM_MODEL", "meta-llama/Meta-Llama-3.1-8B-Instruct")
+BASE_URL = ""
+API_KEY = ""
 MODEL = "Mistral-Small-3.2-24B-Instruct-2506"
 
 client = OpenAI(base_url=BASE_URL, api_key=API_KEY)
